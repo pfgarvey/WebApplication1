556362925c23322cbfcbf3e3574e3fd7ae26b851
Pat Garvey <pfgarvey@gmail.com>
2021-06-18 11:30:52 -0400

    kelvion

--- WebApplication1/WeatherForecast.cs
+++ WebApplication1/WeatherForecast.cs
@@ -10,6 +10,7 @@ namespace WebApplication1
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        public int TemperatureK => 273 + TemperatureC;
         public string Summary { get; set; }
     }
 }
